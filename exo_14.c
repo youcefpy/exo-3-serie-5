b91b508ce2d94956c1d9cca10cca00922b224d68
BOUTICHE YOUCEF <78690790+youcefpy@users.noreply.github.com>
2024-03-17 23:36:11 +0100

    Update exo_14.c

--- exo_14.c
+++ exo_14.c
@@ -1,34 +1,53 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-int main() {
-    // Write C code here
-   int A[3][3], v[3],B[3][1],p;
-   p = 0 ; 
-   for(int i = 0 ;i<3;i++){
-       for(int j = 0 ;j<3;j++){
-           printf("A[%d][%d] : ",i,j);
-           scanf("%d",&A[i][j]);
-       }
-   }
-   
-   printf("\nremplir le vecteur v :\n");
-   for(int i = 0;i<3;i++){
-       printf("v[%d] : ",i);
-       scanf("%d",&v[i]);
-   }
-   
-   for(int i = 0 ;i<3;i++){
-       p = 0;
-       for(int j = 0 ;j<3;j++){
-           p= p + (A[i][j] * v[j]);
-       }
-       B[i][0] = p ;
-   }
-   
-   for(int i = 0 ;i<3;i++){
-       printf("%d\n",B[i][0]);
-   }
 
+int main() {
+    
+    int a[50][50],v[50],n,m,b[50],p;
+    p=0;
+    printf("donnez le nombre des ligne de la matrice: ");
+    scanf("%d",&n);
+    printf("donnez le nombre des colonne de la matrice: ");
+    scanf("%d",&m);
+    
+    printf("remplissage de la matrice : \n");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("a[%d][%d]",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\nremlissage du vecteur : ");
+    for(int i=0;i<m;i++){
+        printf("v[%d]: ",i);
+        scanf("%d",&v[i]);
+    }
+    
+    for(int i =0;i<n;i++){
+      p=0;
+        for(int j=0;j<m;j++){
+            p = p+ (a[i][j]*v[j]);
+        }
+        b[i]=p;
+    }
+    printf("matrice a: \n");
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("%d\t",a[i][j]);
+            
+        }
+        printf("\n");
+    }
+    
+    printf("vecteur v : \n");
+    for(int i =0;i<m;i++){
+        printf("%d\t",v[i]);
+    }
+    printf("\n produit vecteur matrice : ");
+
+    for(int i =0;i<n;i++){
+        printf("%d\t",b[i]);
+    }
     return 0;
 }
